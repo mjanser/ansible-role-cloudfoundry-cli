5892aac9361eec315b258bed975e2d177402ff92
Martin Janser <martin@duss-janser.ch>
2015-11-26 13:48:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Ansible Role: CloudFoundry CLI
+
+An Ansible role that installs the CloudFoundry CLI on Ubuntu.
+
+## Requirements
+
+None
+
+## Role Variables
+
+None
+
+## Dependencies
+
+None
+
+## Example Playbook
+
+    - hosts: all
+      roles:
+        - { role: mjanser.cloudfoundry-cli }
+
+## License
+
+MIT
---
+++ tests/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ansible-playbook playbook.yml --syntax-check || exit 1
+
+ansible-playbook playbook.yml --connection=local || exit 1
+
+cf --version
+
+ansible-playbook playbook.yml --connection=local | tee /tmp/idempotence.log
+grep -q "changed=0.*failed=0" /tmp/idempotence.log \
+    && (echo "Idempotence test: pass" && exit 0) \
+    || (echo "Idempotence test: fail" && exit 1)
